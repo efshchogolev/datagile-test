2826d045f40639a706e54f8b3026459463b25d17
Efim Shchegolev <efimblashck@yandex.ru>
2024-10-18 16:57:43 +0300

    fix: add base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/datagile-test/',
   plugins: [react(), svgr()],
   css: {
     modules: {
